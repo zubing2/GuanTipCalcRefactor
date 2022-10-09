7596be804d75d1115302b82313683b0a3dd6d5ed
Zubin Guan <zubing2@nycstudents.net>
2022-10-09 00:29:12 -0400

    final commit

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,33 +1,53 @@
 public class TipCalculator {
 
+    // declaring the instance variables
     private int numPeople;
     private double tipPercent;
     private double totalBillBeforeTip;
 
+    // main constructor
     public TipCalculator(int numPeople, double tipPercent) {
         this.numPeople = numPeople;
         this.tipPercent = tipPercent;
         this.totalBillBeforeTip = 0.0;
     }
 
-    public int getNumPeople() {
-        return numPeople;
-    }
-
+    // getter methods
     public double getTipPercent() {
         return tipPercent;
     }
-
     public double getTotalBillBeforeTip() {
         return totalBillBeforeTip;
     }
 
+    // adds the cost of each item to the total price
     public void getMeal(double cost) {
-         totalBillBeforeTip += cost;
+        totalBillBeforeTip += cost;
     }
 
+    // returns the amount that is being tipped in decimal form
     public double tipAmount() {
-        return (totalBillBeforeTip / tipPercent);
+        return (totalBillBeforeTip * (tipPercent / 100.00));
+    }
+
+    // returns total bill amount including tip
+    public double totalBill() {
+        return (totalBillBeforeTip + tipAmount());
+    }
+
+    // returns the cost per person before tip
+    public double costPerPersonBeforeTip() {
+        return totalBillBeforeTip / numPeople;
+    }
+
+    // returns the amount of tip each person pays
+    public double tipAmountPerPerson() {
+        return tipAmount() / numPeople;
+    }
+
+    // returns the final cost per person with tip
+    public double totalCostPerPerson() {
+        return totalBill() / numPeople;
     }
 }
 
--- src/TipCalculatorRunner.java
+++ src/TipCalculatorRunner.java
@@ -1,23 +1,52 @@
+// Import of Classes
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
 public class TipCalculatorRunner {
     public static void main(String[] arg) {
         Scanner scan = new Scanner(System.in);
+        DecimalFormat formatter = new DecimalFormat("#.00");
 
+        // Welcoming message
         System.out.println("Welcome to the tip calculator!");
-        System.out.println("How many people are there?");
-        int numPeople = scan.nextInt();
 
-        System.out.println("What's the tip percentage(As a decimal)");
-        double tipPercent = scan.nextDouble();
+        // asking for user input
+        System.out.print("How many people are there?");
+        int people = scan.nextInt();
 
-        boolean moreInputs = true;
+        System.out.print("What's the tip percentage");
+        int tip = scan.nextInt();
 
-        while (moreInputs) {
-            System.out.print("What is the cost of the item? e.g 12.50 (Type -1 to end): ");
+        // New Tip Calculator Object
+        TipCalculator calc = new TipCalculator(people, tip);
 
+        System.out.print("Enter the cost of the item in dollar and cents (Type -1 to end): ");
+        double cost = scan.nextDouble();
+
+        // While loop to enable cost input until -1 is inputted.
+        while (cost != -1) {
+            calc.getMeal(cost);
+            System.out.print("Enter the cost of the item in dollar and cents (Type -1 to end): ");
+            cost = scan.nextDouble();
         }
 
+        // Bill Calculations and formatting to two decimal places.
+        String formattedTotalCost = formatter.format(calc.getTotalBillBeforeTip());
+        String formattedTip = formatter.format(calc.getTipPercent());
+        String formattedTotalTip = formatter.format(calc.tipAmount());
+        String formattedBillWithTip = formatter.format(calc.totalBill());
+        String formattedCostPerPerson = formatter.format(calc.costPerPersonBeforeTip());
+        String formattedTipPerPerson = formatter.format(calc.tipAmountPerPerson());
+        String formattedTotalPerPerson = formatter.format(calc.totalCostPerPerson());
+
+        // Printing out all the formatted info.
+        System.out.println("--------------------");
+        System.out.println("Total Bill Before Tip: " + formattedTotalCost);
+        System.out.println("Tip Percent: " + formattedTip);
+        System.out.println("Total Tip Amount: " + formattedTotalTip);
+        System.out.println("Total Bill Including Tip: " + formattedBillWithTip);
+        System.out.println("Cost Per Person Before Tip: " + formattedCostPerPerson);
+        System.out.println("Tip Per Person: " + formattedTipPerPerson);
+        System.out.println("Final Amount Per Person: " + formattedTotalPerPerson);
     }
 }
